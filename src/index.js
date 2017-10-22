cad4d9221bbdd75ddcb58e5965bb3378030ebcb3
Ivan Yudo <iyudo26@gmail.com>
2017-10-22 15:59:30 +0700

    webpack assets management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+// import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
 	var element = document.createElement('div');
 	// this statement needs to include Loadash library to work
 	element.innerHTML = _.join(['Hello', 'Webpack'], '');
+	element.classList.add('hello');
+
+	// var myImg = document.createElement('img');
+	// myImg.src = Icon;
+
+	// element.appendChild(myImg);
+
+	console.log(Data);
+
 	return element;
 }
 
